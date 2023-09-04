fb4f7d04c74a6db6062c63f7cc03cc5ebcb8e23e
calberto-payall <cserrano@payall.com.ve>
2023-09-04 11:46:42 -0400

    lesson 10 transiciones

---
+++ src/Animaciones.php
@@ -0,0 +1,10 @@
+<?php
+include "../templates/header.html"
+?>
+    <h1 class="center shadowEffect">Animaciones en CSS</h1>
+    <div id="animacion">
+
+    </div>
+<?php
+include "../templates/footer.html";
+?>
