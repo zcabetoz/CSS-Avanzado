33a9f5a2502d60ef6a48dcaefca37d32de7cf6a7
calberto-payall <cserrano@payall.com.ve>
2023-09-05 17:43:16 -0400

    lesson 20 unidades de medida

---
+++ src/Medidas.php
@@ -0,0 +1,24 @@
+<?php
+include "../templates/header.html";
+?>
+    <div class="cat-shop">
+        <div class="col">
+            <h1 class="cont-title">
+                TIENDA PARA GATOS
+            </h1>
+            <p class="cont-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dignissimos distinctio ea eaque eum
+                itaque modi neque, nostrum quam quia. Accusantium ipsum nobis numquam repellendus similique. At aut
+                autem consectetur, consequuntur doloremque dolorum eius eum eveniet ex, expedita explicabo hic in labore
+                magni nemo nisi nulla numquam officia quibusdam quis, quos sed similique sunt suscipit ut velit veniam
+                vero voluptatum.
+            </p>
+            <a class="btn" href="#">compra ya!</a>
+        </div>
+        <div class="col">
+            <img class="img-gato" src="../public/assets/images/gato_agora.jpg" alt="gato.jpg">
+        </div>
+    </div>
+<?php
+include "../templates/footer.html"
+?>
---
+++ src/Menu.php
@@ -0,0 +1,26 @@
+<?php
+include "../templates/header.html";
+?>
+<div class="c-body">
+    <div class="c-header">
+        <h1 class="c-logo">
+            <a href="#">Logo</a>
+        </h1>
+        <div class="c-header-nav">
+            <nav class="c-nav">
+                <ul>
+                    <li><a href="#">BLOGS</a></li>
+                    <li><a href="#">CURSOS</a></li>
+                    <li><a href="#">NOTICIAS</a></li>
+                </ul>
+            </nav>
+            <a href="#" class="c-user">
+                <span>USUARIO</span>
+                <img class="imageAsh" src="../public/assets/images/ash.jpg" alt="user.jpg">
+            </a>
+        </div>
+    </div>
+</div>
+<?php
+include "../templates/footer.html"
+?>
